296a483382be60b3c402e16b98ddcd3407753b06
fywc <imfywc@gmail.com>
2023-09-09 11:34:47 +0800

    finish Round 886 Problem H

--- Div.4/886/H_The_Third_Letter.cpp
+++ Div.4/886/H_The_Third_Letter.cpp
@@ -1,10 +1,57 @@
 #include <bits/stdc++.h>
 
+#define ll long long
+#define PII pair<ll, ll>
+
 using namespace std;
+
+const int INF = 0x3f3f3f3f;
 
 void Solution()
 {
+        int n, m;
+        ll a, b, d;
+        cin >> n >> m;
+        vector<PII> g[n + 1];
+        ll dist[n + 1];
+        
+        for (int i = 0; i <= n; i++) {
+                dist[i] = INF;
+        }
+        for (int i = 0; i < m; i++) {
+                cin >> a >> b >> d;
+                g[a].emplace_back(b, d);
+                g[b].emplace_back(a, -d);
+        }
 
+        function<bool(int)> dfs = [&](int u) -> bool {
+                for (auto [x, y] : g[u]) {
+                    if (dist[x] != INF) {
+                        if (dist[x] != dist[u] + y) {
+                            return false;
+                        }
+                    } else {
+                        dist[x] = dist[u] + y;
+                        if (!dfs(x)) {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+        };
+
+        for (int i = 1; i <= n; ++i) {
+                if (dist[i] != INF) {
+                    continue;
+                }
+                dist[i] = 0;
+                if (!dfs(i)) { 
+                    cout << "NO" << endl;
+                    return;
+                }
+            }
+        cout << "YES" << endl;
+        return ;
 }
 
 int main()
@@ -14,5 +61,6 @@ int main()
     while(T--) {
         Solution();
     }
-    return 0;
+    return (int)0;
 }
+
